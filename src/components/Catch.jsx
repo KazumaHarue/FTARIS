50c3faabe62a26d2f7ef56e4b132337c37ada834
harue <harue1477@gmail.com>
2021-01-25 17:29:34 +0900

    [update]オンライン人数を追加
    オンライン人数を表示できるようにしました。

--- src/components/Catch.jsx
+++ src/components/Catch.jsx
@@ -6,7 +6,10 @@ const Catch = (props) => {
   return (
     <div className="catch row">
       {/* imgに直接hight:autoを指定すると、safariで画像が縦に伸びる */}
-      <div className="col-2"></div>
+      <div className="online col-2">
+        <p className="txt">オンライン</p>
+        <p className="num">{}人</p>
+      </div>
       <img src={Logo} alt="ロゴ" className="col-8" />
       <div className="beat-list col-2" onClick={() => props.selectBeatList()}>
         <div className="logo">
