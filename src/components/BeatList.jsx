2ede1e503c7b47e519be346306684029d5a1f980
harue <harue1477@gmail.com>
2021-01-26 02:59:46 +0900

    [add]BeatList画面の追加
    ビートを一覧で表示されるようにしました。

--- src/components/BeatList.jsx
+++ src/components/BeatList.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/style.css';
 import Header from './Header.jsx';
 
 const BeatList = (props) => {
+  let iconClass = "far fa-play-circle";
+  const [display, btnClick] = useState(false);
+  if (display === true) {
+    iconClass = "far fa-stop-circle";
+  } else {
+    iconClass = "far fa-play-circle";
+  }
   return (
     <div className="beatlist">
-      <p>beatlist</p>
+      {/* TODO: <Header /> 戻るボタンの表示非表示をpropsで受け取る */}
+      <div className="provider"><p>ゆの(NA3TY)</p></div>
+      <div className="beat">
+        <i className={iconClass} onClick={() => btnClick(!display)}></i>
+        <p className="name">BPM94(戦国MCBATTLE提供)</p>
+      </div>
     </div>
   )
 };
