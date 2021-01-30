9aaa7f0a409f9fdceaaaf12bce665882c5743307
harue <harue1477@gmail.com>
2021-01-30 22:23:31 +0900

    [add]ボタンの追加
    ButtonGroupにボタンを追加しました。

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -4,10 +4,16 @@ import '../styles/style.css';
 // props.selectBattle : BattlePageに遷移し、マッチングの処理を行う
 
 const ButtonGroup = (props) => (
-  <div className="buttongroup row justify-content-center">
-    <button type="button" className="startbutton btn btn-primary btn-lg w-25" onClick={() => props.selectBattle()}>
-      バトルを始める
+  <div className="buttongroup row">
+    {/* <button type="button" className="btn btn-primary">
+      ○
       </button>
+    <button type="button" className="btn btn-primary">
+      ○
+      </button>
+    <button type="button" className="btn btn-primary">
+      ○
+      </button> */}
     <button type="button" className="startbutton btn btn-primary btn-lg w-100" onClick={() => props.selectBattle()}>
       バトルを始める
       </button>
